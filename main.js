a745784f7b41d1b363357a0857504ef57d2af060
Pedro <pedrobarbosar12@gmail.com>
2023-01-31 10:14:10 -0300

    alterando

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ Crie um algoritmo que transforme as notas do sistema numérico para sistema de n
 
 */
 // Primeiro Passo: criar e determinar as variáveis
-
-let note = document.querySelector('input');
+let note = document.querySelector('#revealNote').value;
 // Segundo Passo: criar condições de acordo com as notas
 // Se a nota for 90 ou maior, mostra-se A
 // Se a nota for entre 89 e 89, mostra-se B
 // Se a nota for entre 70 e 79, mostra-se C
 // Se a nota for entre 60 e 69, mostra-se D
 // Se a nota for menor que 60, mostra-se F
+
