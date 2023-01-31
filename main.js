6b0f5fb70e40ccb125f81f46e94afd9a1589cc7a
Pedro <pedrobarbosar12@gmail.com>
2023-01-31 10:49:06 -0300

    :tada: adicionando logica

--- main.js
+++ main.js
@@ -9,12 +9,43 @@ Crie um algoritmo que transforme as notas do sistema numérico para sistema de n
 * menor que 60 - F
 
 */
-// Primeiro Passo: criar e determinar as variáveis
-let note = document.querySelector('#revealNote').value;
-// Segundo Passo: criar condições de acordo com as notas
-// Se a nota for 90 ou maior, mostra-se A
-// Se a nota for entre 89 e 89, mostra-se B
-// Se a nota for entre 70 e 79, mostra-se C
-// Se a nota for entre 60 e 69, mostra-se D
-// Se a nota for menor que 60, mostra-se F
+
+
+function getScore(score) {
+  let scoreA = score >= 90 && score <= 100;
+  let scoreB = score >= 80 && score <= 89;
+  let scoreC = score >= 70 && score <= 79;
+  let scoreD = score >= 60 && score <= 69;
+  let scoreF = score < 60 && score >= 0;
+
+  let scoreFinal;
+
+  if (scoreA) {
+    scoreFinal = "A";
+  } else if (scoreB) {
+    scoreFinal = "B";
+  } else if (scoreC) {
+    scoreFinal = "C";
+  } else if (scoreD) {
+    scoreFinal = "D";
+  } else if (scoreF) {
+    scoreFinal = "F";
+  } else {
+    scoreFinal = "Nota inválida";
+  }
+
+  return scoreFinal
+}
+
+
+console.log(getScore(101))
+console.log(getScore(-1))
+console.log(getScore(0))
+console.log(getScore(1))
+console.log(getScore(45))
+console.log(getScore(60))
+console.log(getScore(61))
+console.log(getScore(75))
+console.log(getScore(85))
+console.log(getScore(95))
 
